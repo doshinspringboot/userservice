ddcdca77d26e416d5a686515039c193c33476bbc
Geo Doshin Wills <doshin.wills@gmail.com>
2018-01-22 16:57:12 -0800

    for eureka client

--- src/main/java/com/doshin/UserserviceApplication.java
+++ src/main/java/com/doshin/UserserviceApplication.java
@@ -3,9 +3,11 @@ package com.doshin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.metrics.rich.InMemoryRichGaugeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class UserserviceApplication {
 	
