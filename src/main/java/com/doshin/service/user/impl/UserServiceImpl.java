12d7eda809f1e15ba1dd9d8ab1e7892e204e86d2
Geo Doshin Wills <doshin.wills@gmail.com>
2018-01-25 17:25:19 -0800

    no message

--- src/main/java/com/doshin/service/user/impl/UserServiceImpl.java
+++ src/main/java/com/doshin/service/user/impl/UserServiceImpl.java
@@ -1,6 +1,8 @@
 package com.doshin.service.user.impl;
 
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -53,8 +56,14 @@ public class UserServiceImpl {
 	}
 
 	@GetMapping(value = "/user/{userid}", produces = MediaType.APPLICATION_XML_VALUE)
-	public @ResponseBody  UserVO findByUserId(@PathVariable("userid") Integer userId) {
+	public @ResponseBody  UserVO findByUserId(@PathVariable("userid") Integer userId, 
+			@RequestHeader("x-forwarded-host") Optional<String> host, 
+			@RequestHeader("x-forwarded-port") Optional<String> port) {
 		logger.info("User service started for : "  +userId );
+		if(host.isPresent() && port.isPresent()) {
+			logger.info("User service called from proxy service : "  + host.get() + " port : " + port.get());
+		}
+
 		userGauageService.increment("userid.call.count");
 		Long start = System.nanoTime();
 		UserVO user = userBao.findByUserId(userId);
